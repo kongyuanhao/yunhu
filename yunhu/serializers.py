6d5320dec43e39ff2adbcba9a5e6116980f9febc
kongyuanhao <34028821+kongyuanhao@users.noreply.github.com>
2018-04-21 18:08:54 +0800

    customer list

--- yunhu/serializers.py
+++ yunhu/serializers.py
@@ -7,21 +7,24 @@
 from rest_framework import serializers
 from models import *
 
+
 # 认证方式 序列化
 class CheckWayModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = CheckWayModel
         fields = "__all__"
 
+
 # 渠道 序列化
 class ChannelModelSerializer(serializers.ModelSerializer):
-    check_ways_get = CheckWayModelSerializer(source="check_ways",many=True,read_only=True)
+    check_ways_get = CheckWayModelSerializer(source="check_ways", many=True, read_only=True)
     link_h5 = serializers.ReadOnlyField()
 
     class Meta:
         model = ChannelModel
-        fields = ["id","name","link_h5","check_ways_get","create_time",'company',"check_ways"]
-        write_only_fields = ('company',"check_ways")
+        fields = ["id", "name", "link_h5", "check_ways_get", "create_time", 'company', "check_ways"]
+        write_only_fields = ('company', "check_ways")
+
 
 # 员工管理 序列化
 class UserSerializer(serializers.ModelSerializer):
@@ -30,23 +33,50 @@ class UserSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
     def create(self, validated_data):
-        instance = super(UserSerializer,self).create(validated_data)
-        if validated_data["password"]:
-            instance.set_password(validated_data["password"])
-            instance.save()
-        return instance
-    def update(self, instance, validated_data):
-        instance = super(UserSerializer, self).create(instance,validated_data)
+        instance = super(UserSerializer, self).create(validated_data)
         if validated_data["password"]:
             instance.set_password(validated_data["password"])
             instance.save()
         return instance
 
+    def update(self, instance, validated_data):
+        instance = super(UserSerializer, self).create(instance, validated_data)
+        if validated_data["password"]:
+            instance.set_password(validated_data["password"])
+            instance.save()
+        return instance
+
+
 # 客户管理
 class CustomerModelListSerializer(serializers.ModelSerializer):
+    audit_user = serializers.SerializerMethodField()
+    loan_user = serializers.SerializerMethodField()
+    urge_user = serializers.SerializerMethodField()
+    channel__name = serializers.CharField(source='channel.name')
     class Meta:
         model = CustomerModel
-        fields = ["id","name","tel","identity","zhima_score","wechat","zone","address"]
+        fields = ["id", "channel__name", "name", "tel", "identity", "zhima_score", "wechat", "zone", "address",
+                  "audit_user", "loan_user", "urge_user"]
+
+    def get_audit_user(self, obj):
+        user  = obj.audit_customer.all()
+        if user:
+            return user[0].user.name
+        return ""
+
+    def get_loan_user(self, obj):
+        user = obj.lona_customer.all()
+        if user:
+            return user[0].user.name
+        return ""
+
+    def get_urge_user(self, obj):
+        user = obj.urge_customer.all()
+        if user:
+            return user[0].user.name
+        return ""
+
+
 class CustomerModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomerModel
